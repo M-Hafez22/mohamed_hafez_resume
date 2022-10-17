8e83ae400c380a05328447fc06a49e8c577d8b8f
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-10-17 14:52:18 +0200

    Convert Html to pdf script

---
+++ htmlToPdf.py
@@ -0,0 +1,14 @@
+import pdfkit
+
+# Define path to wkhtmltopdf
+path_to_wkhtmltopdf = r'/bin/wkhtmltopdf'
+
+# Define path to HTML file
+path_to_file = 'mohamed_hafez_resume.html'
+
+# Point pdfkit configuration to wkhtmltopdf
+config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+
+# Convert HTML file to PDF
+pdfkit.from_file(path_to_file, output_path='mohamed_hafez_resume.pdf', configuration=config)
